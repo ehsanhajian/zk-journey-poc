3d7575947a6bf6f60782c776c6914d37770f2380
codespool <4625220+codespool@users.noreply.github.com>
2023-11-14 15:08:24 +0200

    add tw key env

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { json, LoaderFunction, type LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,6 +7,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
 import {
@@ -22,7 +23,32 @@ import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
+export async function loader() {
+  // console.group(process.env.TW_API_KEY);
+  return json({ twApiKey: process.env.TW_API_KEY });
+}
+
+const zKatana = {
+  // === Required information for connecting to the network === \\
+  chainId: 1261120, // Chain ID of the network
+  // Array of RPC URLs to use
+  rpc: ["https://rpc.zkatana.gelato.digital"],
+  // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+  // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+  nativeCurrency: {
+    decimals: 18,
+    name: "Sepolia ETH",
+    symbol: "ETH",
+  },
+  shortName: "zKatana", // Display value shown in the wallet UI
+  slug: "consensys", // Display value shown in the wallet UI
+  testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+  chain: "zKatana ", // Name of the network
+  name: "Astar zKatana Testnet", // Name of the network
+};
+
 export default function App() {
+  const { twApiKey } = useLoaderData<LoaderFunction>();
   return (
     <html lang="en">
       <head>
@@ -34,8 +60,8 @@ export default function App() {
       </head>
       <body>
         <ThirdwebProvider
-          activeChain="mumbai"
-          clientId="YOUR_CLIENT_ID"
+          activeChain={zKatana}
+          clientId={twApiKey}
           supportedWallets={[
             metamaskWallet(),
             walletConnect(),
