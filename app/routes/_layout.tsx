4e8fb78423c120576a38aa0a89e3fecdc36ac330
codespool <4625220+codespool@users.noreply.github.com>
2023-12-20 01:03:49 +0200

    update layout

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -1,10 +1,22 @@
 import { Outlet } from "@remix-run/react";
-import { NavBar } from "../components/NavBar";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { BridgeIcon, CardIcon, FolderIcon, LanguageIcon, PoolIcon } from "../components/Icons";
 
 export default function Layout() {
   return (
     <>
-      <NavBar />
+      <div className="absolute top-0 right-0 p-4 z-50">
+        <ConnectButton />
+      </div>
+      <div className="absolute left-[1rem] top-[3rem] flex flex-col justify-start space-y-5 items-center p-4 bg-opacity-50 z-50">
+        <BridgeIcon />
+        <CardIcon />
+        <FolderIcon />
+        <PoolIcon />
+        <div className="justify-self-end">
+          <LanguageIcon />
+        </div>
+      </div>
       <Outlet />
     </>
   );
