6d0b5d71e86c02189275fcf57ecd9f8fe62aab81
codespool <4625220+codespool@users.noreply.github.com>
2023-12-19 23:38:21 +0200

    add evolve and notebook pages

---
+++ app/routes/baths.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { LoaderFunction, json } from "@remix-run/node";
+import { stringify } from "qs";
+import { strapiLoader } from "~/helpers/strapiLoader";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async () => {
+  const query = stringify({
+    populate: {
+      background_image: {
+        fields: ["url", "name", "formats"],
+      },
+    },
+  });
+  const apiData = await strapiLoader("/baths-page", query);
+  return json(apiData);
+};
+
+export default function Evolve() {
+  const { apiData, imageUrlPrefix } = useLoaderData<LoaderFunction>();
+
+  const backgroundImage = apiData.data.attributes.background_image.data.attributes.url;
+  const fullImageUrl = `${imageUrlPrefix}${backgroundImage}`;
+
+  return (
+    <div
+      className="bg-cover bg-center h-screen w-full flex items-center justify-center flex-col"
+      style={{ backgroundImage: `url(${fullImageUrl})` }}
+    />
+  );
+}
